5566fe0bd7e2fb6f4c3db01619f41f7b9662483f
par_amour <71580390+wangzupeng12061@users.noreply.github.com>
2023-05-12 21:36:15 +0800

    Merge remote-tracking branch 'origin/main'

---
+++ Codes/src/TranslationResult.java
@@ -0,0 +1,25 @@
+package src;
+
+public class TranslationResult {
+    private String sourceText;
+    private String translatedText;
+    private String targetLanguage;
+
+    public TranslationResult(String sourceText, String translatedText, String targetLanguage) {
+        this.sourceText = sourceText;
+        this.translatedText = translatedText;
+        this.targetLanguage = targetLanguage;
+    }
+
+    public String getSourceText() {
+        return this.sourceText;
+    }
+
+    public String getTranslatedText() {
+        return this.translatedText;
+    }
+
+    public String getTargetLanguage() {
+        return this.targetLanguage;
+    }
+}
