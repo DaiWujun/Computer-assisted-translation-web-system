c63b673fa3177d3ef217bebe6779cf376b9e453c
Tsubaki-01 <wengqiang9198@gmail.com>
2023-06-02 16:07:42 +0800

    edit

--- Codes/BackEnd/Architecture/API_LangDetect.py
+++ Codes/BackEnd/Architecture/API_LangDetect.py
@@ -1,17 +1,21 @@
 from flask import Flask, jsonify, request
 from langdetect import detect
+from gevent import pywsgi
 
 app = Flask(__name__)
 
-@app.route('/detect-language', methods=['POST'])
+
+@app.route('/detect_language', methods=['GET'])
 def detect_language():
     data = request.get_json()
     text = data['text']
+
     try:
         language = detect(text)
         return jsonify({'language': language})
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
-if __name__ == '__main__':
-    app.run()
+
+
+
